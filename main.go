c8a162ae7dbbb442ae939faa46794e9d436d34d0
savasozturk <savas.ozturk@gmail.com>
2018-05-30 10:00:06 +0300

    initiate!

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+//Customer bla bla
+type Customer struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+
+	customer := Customer{"Savas", "Ozturk"}
+	_ = customer
+
+	customer2 := Customer{}
+	customer2.firstName = "Derin"
+	customer2.lastName = "Ozturk"
+
+	n, err := fmt.Printf("customerName:%v\n", customer2)
+	if err == nil {
+		fmt.Printf("n:%+v", n)
+	}
+}

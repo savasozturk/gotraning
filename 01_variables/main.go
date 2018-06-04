1fdfa5f2f84407f5476f7b37f9fc882e09157a3a
savasozturk <savas.ozturk@gmail.com>
2018-06-04 12:09:36 +0300

    Added id

--- 01_variables/main.go
+++ 01_variables/main.go
@@ -4,12 +4,13 @@ import "fmt"
 
 //Customer comment not to throw warning!
 type Customer struct {
+	id        int
 	firstName string
 	lastName  string
 }
 
 func main() {
-	customer := Customer{"Savas", "Ozturk"}
+	customer := Customer{7, "Savas", "Ozturk"}
 	n, err := fmt.Printf("customerName:%v\n", customer)
 	if err == nil {
 		fmt.Printf("n:%+v", n)
